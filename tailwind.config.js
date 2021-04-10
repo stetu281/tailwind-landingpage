b31df6dd121c46a99657bfa03b0390cebe6216fb
Stefan Turner <turner.stefan281@gmail.com>
2021-04-10 09:12:48 +0200

    Clean Start

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'raleway': ['Raleway', 'sans-serif'],
+      'openSans': ['Open Sans', 'sans-serif']
+    },
+    colors: {
+      blue: {
+        dark: 'hsl(243, 87%, 12%)',
+        desatured: 'hsl(238, 22%, 44%)',
+        bright: 'hsl(224, 93%, 58%)',
+        cyan: 'hsl(170, 45%, 43%)'
+      },
+      gray: {
+        lightGrayBlue: 'hsl(240, 75%, 98%)',
+        light: 'hsl(0, 0%, 75%)',
+      }
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
